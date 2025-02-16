0305ddee3587bbb249872c0f4f823e09498bed2d
axtro112 <c22-4587-01@spist.edu.ph>
2025-02-17 07:56:13 +0900

    removal
    remove unnecessary  features

--- score.js
+++ score.js
@@ -7,25 +7,9 @@ function homeincrement1(){
     counthome += 1
     homescoreEl.textContent = counthome   
 }
-function homeincrement2(){
-    counthome += 2
-    homescoreEl.textContent = counthome    
-}
-function homeincrement3(){
-    counthome += 3
-    homescoreEl.textContent = counthome    
-}
 function awayincrement1(){
     countaway += 1
     scoreawayEL.textContent = countaway  
-}
-function awayincrement2(){
-    countaway += 2
-    scoreawayEL.textContent = countaway    
-}
-function awayincrement3(){
-    countaway += 3
-    scoreawayEL.textContent = countaway   
 }
 function resetaway(){
     scoreawayEL.textContent = 0
