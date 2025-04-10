681289dd68c699d06d4104ff768060f26ebd6a40
axtro112 <c22-4587-01@spist.edu.ph>
2025-04-11 03:38:31 +0900

    started  a new project
    started a  new project for school

---
+++ testjjs.js
@@ -0,0 +1,14 @@
+// const primarynav =  document.querySelector(".primaryNavigation");
+// const navToogle = document.querySelector(".togglemobile");
+
+// navToogle.addEventListener("click", () => {
+//     const visibility = primarynav.getAttribute("data-visible");
+     
+//      if (visibility === "false"){
+//         primarynav.setAttribute("data-visible", true);
+//         navToogle.setAttribute("aria-expanded", true);
+//      }else{
+//         primarynav.setAttribute("data-visible", false);
+//         navToogle.setAttribute("aria-expanded", false);
+//      }
+// });
